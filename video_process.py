2221c57209cb7f81ea631d04e61f8cd837c0902b
harsh.kakashaniya <harsh.kakashniya@gmail.com>
2019-02-24 21:38:31 -0500

    Refactor:Code split in functions

--- video_process.py
+++ video_process.py
@@ -3,6 +3,7 @@ import numpy as np
 import os, sys
 from numpy import linalg as LA
 import math
+from PIL import Image
 try:
     sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 except:
@@ -14,27 +15,29 @@ import cv2
 def Extractedges(image,lower,upper):
     mask = cv2.inRange(image, lower, upper)
     #print(np.shape(mask)
-        '''
+    '''
     for i in range(height):
         for j in range(width):
             if( i<70 or i>height-70 or j<70 or j>width-70 ):
                 filter[i,j]=0
     filtered = cv2.bitwise_and(image, image, mask = filter)
-        '''
-
-    corners = cv2.goodFeaturesToTrack(mask,50,0.01,1)
+    '''
+    corners = cv2.goodFeaturesToTrack(mask,50,0.001,1)
     corners = np.int0(corners)
 
-    j=0
+    #cv2.namedWindow("images_mask", cv2.WINDOW_NORMAL)
+    #cv2.imshow("images_mask", mask)
+    #cv2.waitKey()
+    j=50
     a=0
-    while (j<len(mask[0])-10):
-        for i in range(0,len(mask)-10):
+    while (j<len(mask[0])-50):
+        for i in range(50,len(mask)-50):
             #print(i,j)
-            if(mask[i,j]==0 and mask[i+10,j+10]==0 and a==0):
+            if(mask[i,j]==0 and mask[i+5,j+5]==0 and mask[i+10,j+10]==0 and a==0):
                 a=i
                 b=j
                 #print('got white point',a,b)
-            if(mask[i,j]==0 and mask[i+10,j+10]==0):
+            if(mask[i,j]==0 and mask[i+5,j+5]==0 and mask[i+10,j+10]==0):
                 c=i
                 d=j
 
@@ -51,8 +54,30 @@ def Extractedges(image,lower,upper):
             corners[i,0,1]=0
     return image,corners
 
+def cornerpoint(a_x_old,a_y_old,edge_matrix):
+    difference_x=abs(a_x_old-edge_matrix[:,0])
+    if(min(difference_x)<10):
+        a_x=edge_matrix[int(np.argmin(difference_x)),0]
+        a_y=edge_matrix[int(np.argmin(difference_x)),1]
+    else:
+        a_x=a_x_old
+        a_y=a_y_old
+    return a_x ,a_y
 
-def edgesplotter(corners):
+
+def initializecorners(edge_matrix):
+    a_x_old=edge_matrix[0,0]
+    a_y_old=edge_matrix[0,1]
+    b_x_old=edge_matrix[2,0]
+    b_y_old=edge_matrix[2,1]
+    c_x_old=edge_matrix[1,0]
+    c_y_old=edge_matrix[1,1]
+    d_x_old=edge_matrix[3,0]
+    d_y_old=edge_matrix[3,1]
+
+    return a_x_old,a_y_old,b_x_old,b_y_old,c_x_old,c_y_old,d_x_old,d_y_old
+
+def edgesplotter(src,image,corners,count,a_x_old,a_y_old,b_x_old,b_y_old,c_x_old,c_y_old,d_x_old,d_y_old):
     corner_x_min_x=10000
     corner_x_min_y=10000
     corner_x_max_x=0
@@ -61,7 +86,6 @@ def edgesplotter(corners):
     corner_y_min_y=10000
     corner_y_max_x=0
     corner_y_max_y=0
-
     for i in range (1,len(corners)):
         corner_x=corners[i,0,0]
         corner_y=corners[i,0,1]
@@ -82,14 +106,45 @@ def edgesplotter(corners):
             corner_y_max_x=corners[i,0,0]
             corner_y_max_y=corners[i,0,1]
 
+    edge_matrix=np.mat(([corner_x_min_x,corner_x_min_y],[corner_x_max_x,corner_x_max_y],
+    [corner_y_min_x,corner_y_min_y],[corner_y_max_x,corner_y_max_y]))
 
-    cv2.circle(image,(corner_x_min_x,corner_x_min_y),1,(0,255,0),5)
-    cv2.circle(image,(corner_y_min_x,corner_y_min_y),1,(0,255,0),5)
-    cv2.circle(image,(corner_x_max_x,corner_x_max_y),1,(255,0,0),5)
-    cv2.circle(image,(corner_y_max_x,corner_y_max_y),1,(255,0,0),5)
+    print(edge_matrix)
 
-    slope_max=math.atan(abs(corner_x_max_y-corner_y_max_y)/abs(corner_x_max_x-corner_y_max_x))*(7*180)/22
-    slope_min=math.atan(abs(corner_x_min_y-corner_y_min_y)/abs(corner_x_min_x-corner_y_min_x))*(7*180)/22
+    if(count==0):
+        a_x_old,a_y_old,b_x_old,b_y_old,c_x_old,c_y_old,d_x_old,d_y_old=initializecorners(edge_matrix)
+
+    a_x,a_y=cornerpoint(a_x_old,a_y_old,edge_matrix)
+    b_x,b_y=cornerpoint(b_x_old,b_y_old,edge_matrix)
+    c_x,c_y=cornerpoint(c_x_old,c_y_old,edge_matrix)
+    d_x,d_y=cornerpoint(d_x_old,d_y_old,edge_matrix)
+
+    a_x_old=a_x
+    b_x_old=b_x
+    c_x_old=c_x
+    d_x_old=d_x
+
+    cv2.circle(image,(a_x,a_y),1,(0,255,0),5)
+    cv2.circle(image,(b_x,b_y),1,(0,255,0),5)
+    cv2.circle(image,(c_x,c_y),1,(255,0,0),5)
+    cv2.circle(image,(d_x,d_y),1,(255,0,0),5)
+
+    #pts_dst = np.array([[a_x, a_y], [b_x, b_y], [c_x, c_y],[d_x, d_y]])
+    #pts_src = np.array([[0,0],[199, 0],[199, 199],[0,199]],dtype=float);
+
+    #pts_dst = np.array([[200,200], [400,200], [400,400],[400,400]])
+
+    #h, status = cv2.findHomography(pts_src, pts_dst)
+    #print(h)
+    #print(pts_dst)
+    #print(image.shape[1],image.shape[0])
+    #temp = cv2.warpPerspective(src, h, (image.shape[1],image.shape[0]))
+    #cv2.fillConvexPoly(image, pts_dst.astype(int), 0, 16);
+
+    #image = image + temp;
+
+    slope_max=math.atan(abs(b_y-a_y)/abs(b_x-a_x))*(7*180)/22
+    slope_min=math.atan(abs(c_y-d_y)/abs(c_x-d_x))*(7*180)/22
     slope_avg=(slope_max+slope_min)/2
 
     print(slope_avg,'Slope')
@@ -97,14 +152,11 @@ def edgesplotter(corners):
     angle_min=str(round(slope_min,2))+'min_slope'
     angle_avg=str(round(slope_avg,2))+'avg_slope'
 
-    edge_matrix=np.mat([corner_x_min_x,corner_x_min_y],[corner_x_max_xcorner_x_max_y],
-    [corner_y_min_x,corner_y_min_y],[corner_y_max_x,corner_y_max_y])
-
-    return angle_max,angle_min,angle_avg,edge_matrix
+    return image,angle_max,angle_min,angle_avg,edge_matrix,a_x_old,a_y_old,b_x_old,b_y_old,c_x_old,c_y_old,d_x_old,d_y_old
 
 
 
-def printslope(image,angle_max,angle_min,angle_avg):
+def printslope(image,angle_max,angle_min,angle_avg,corners):
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(image,angle_max,(10,100), font, 1,(255,255,255),2,cv2.LINE_AA)
     cv2.putText(image,angle_min,(10,200), font, 1,(255,255,255),2,cv2.LINE_AA)
@@ -112,18 +164,18 @@ def printslope(image,angle_max,angle_min,angle_avg):
 
     for i in corners:
         x,y = i.ravel()
-        #cv2.circle(image,(x,y),3,(0,255,0),-1)
+        cv2.circle(image,(x,y),3,(0,255,0),-1)
     return image
 
 #-------------------------------------------------------------------------------
-def Imageprocessor(path):
+def Imageprocessor(path,src):
 
     vidObj = cv2.VideoCapture(path)
     # Used as counter variable
     count = 0
     success = 1
     img_array=[]
-    lower = np.array([10, 20, 10], dtype = "uint8")
+    lower = np.array([10,10, 10], dtype = "uint8")
     upper = np.array([200, 200, 200], dtype = "uint8")
     sizeo=(1080,1920)
     filter=np.array(np.ones(sizeo), dtype = "uint8")
@@ -131,18 +183,32 @@ def Imageprocessor(path):
     b=0
     c=0
     d=0
-    while (success and count<5):
+
+    while (success and count<100):
 
         success, image = vidObj.read()
 
         height,width,layers=image.shape
         size = (width,height)
 
-        image,corner=Extractedges(image,lower,upper)
+        image,corners=Extractedges(image,lower,upper)
 
-        angle_max,angle_min,angle_avg,edge_matrix=edgesplotter(corners)
+        if(count==0):
+            a_x_old=0
+            b_x_old=0
+            c_x_old=0
+            d_x_old=0
+            a_y_old=0
+            b_y_old=0
+            c_y_old=0
+            d_y_old=0
 
-        image=printslope(image,angle_max,angle_min,angle_avg)
+
+        image,angle_max,angle_min,angle_avg,edge_matrix,a_x_old,a_y_old,b_x_old,b_y_old,c_x_old,c_y_old,d_x_old,d_y_old=edgesplotter(src,image,corners,count,a_x_old,a_y_old,b_x_old,b_y_old,c_x_old,c_y_old,d_x_old,d_y_old)
+
+        cv2.imwrite('%d.jpg' %count,image)
+
+        image=printslope(image,angle_max,angle_min,angle_avg,corners)
 
         count += 1
         print(count)
@@ -164,5 +230,7 @@ def video(img_array,size):
 if __name__ == '__main__':
 
     # Calling the function
-    Image,size=Imageprocessor('Tag0.mp4')
+    src=cv2.imread('candy.jpg')
+    #src=cv2.resize(src_raw, dsize=None, fx=0.25, fy=0.25)
+    Image,size=Imageprocessor('Tag0.mp4',src)
     video(Image,size)
